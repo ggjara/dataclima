c1b4e48796a382d2bb740d58d08f4232224c9c99
Gonzalo Jara <gonzalojarasaba@gmail.com>
2023-02-24 10:17:00 -0500

    Add Grid layout

--- src/components/people-preview.js
+++ src/components/people-preview.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 import { GatsbyImage } from 'gatsby-plugin-image'
 import { renderRichText } from 'gatsby-source-contentful/rich-text'
+import Grid2 from '@mui/material/Unstable_Grid2'
 
 import Container from './container'
 import Tags from './tags'
@@ -13,7 +14,26 @@ const ArticlePreview = ({ people }) => {
 
   return (
     <Container>
-      <ul className={styles.articleList}>
+      <Grid2 container spacing={6}>
+        {people.map((person) => {
+          return (
+            <Grid2 item xs={12} sm={6} md={4} lg={3} key={person.id}>
+              <Link to={`/people/${person.slug}`} className={styles.link}>
+                <GatsbyImage alt="" image={person.image.gatsbyImage} />
+                <h2 className={styles.title}>{person.name}</h2>
+              </Link>
+              <div>
+                {person.shortBio?.raw && renderRichText(person.shortBio)}
+              </div>
+              <div className={styles.meta}>
+                <small className="meta">{person.company}</small>
+              </div>
+            </Grid2>
+          )
+        })}
+      </Grid2>
+
+      {/* <ul className={styles.articleList}>
         {people.map((person) => {
           return (
             <li key={person.id}>
@@ -30,7 +50,7 @@ const ArticlePreview = ({ people }) => {
             </li>
           )
         })}
-      </ul>
+      </ul> */}
     </Container>
   )
 }
